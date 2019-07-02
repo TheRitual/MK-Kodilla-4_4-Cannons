ecf2185074a60d014a240ef8fda0eef617bf466b
TheRitual <dev@theritual.eu>
2019-07-02 21:49:56 +0200

    Added "Ascii Art" for cannons Added Cannon creational IDs Added methods to operate over cannons

--- src/com/kodilla/Cannon.java
+++ src/com/kodilla/Cannon.java
@@ -1,20 +1,62 @@
 package com.kodilla;
 
 public class Cannon {
-    private boolean isLoaded;
+    private boolean loaded;
+    private final String id;
 
-    Cannon(){
-        this.isLoaded = false;
+    Cannon(boolean loaded)
+    {
+        this.loaded = loaded;
+        this.id = Long.toString(System.nanoTime() % 10000L);
+        if( this.loaded==true ) {
+            System.out.println("[C" + getId() + "]\u27B0\u25CF\u270F || Created loaded cannon");
+        } else {
+            System.out.println("[C" + getId() + "]\u27B0\u25CB\u270F || Created unloaded cannon");
+        }
     }
 
-    public void fire() {
-        if(this.isLoaded) {
-            System.out.println("Fire!");
-            System.out.println("                  __\n" + " sjw             /  \\\n" + "           .-.  |    |\n" + "   *    _.-'  \\  \\__/\n" + "    \\.-'       \\\n" + "   /          _/\n" + "  |      _  /\"\n" + "  |     /_\\'\n" + "   \\    \\_/\n" + "    \"\"\"\"");
-        } else {
-            System.out.println("Cannon isn't loaded. Loading the cannon!");
-            this.isLoaded = true;
-        }
+    Cannon(){
+        this(false);
+    }
 
+    public String getId() {
+        return id;
+    }
+    public void fire() {
+        if(this.loaded) {
+            System.out.println("[C" + getId() + "]\u27B0\u25CE\u270F\u273a\u2728 =\u2635\u2689 F I R E ! ! !");
+            this.loaded = false;
+        } else {
+            this.loadCanon();
+            this.loaded = true;
+        }
+    }
+
+    public void loadCanon(){
+        if( this.isLoaded() ) {
+            System.out.println("[C" + getId() + "]\u27B0\u25CF\u270F || Canon already loaded");
+        } else {
+            System.out.println("[C" + getId() + "]\u27B0\u25C9\u270F || Loading Canon");
+            this.loaded = true;
+            System.out.println("[C" + getId() + "]\u27B0\u25CF\u270F || Canon loaded");
+        }
+    }
+
+    public boolean isLoaded() {
+        return this.loaded;
+    }
+
+    public void status() {
+        if (this.isLoaded()) {
+            System.out.println("[C" + getId() + "]\u27B0\u25CF\u270F || Canon is loaded");
+        } else {
+            System.out.println("[C" + getId() + "]\u27B0\u25CB\u270F || Canon is unloaded");
+        }
+    }
+
+    public static void text(String txt) {
+        System.out.print("\n\n\u2631\u2632\u2633\u2634\u2635\u2636\u2637 ");
+        System.out.print(txt);
+        System.out.println(" \u2637\u2636\u2635\u2634\u2633\u2632\u2631\n");
     }
 }
--- src/com/kodilla/Main.java
+++ src/com/kodilla/Main.java
@@ -2,21 +2,36 @@ package com.kodilla;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
         List<Cannon> cannons = new LinkedList<>();
+
+        Cannon.text("Creating Cannons");
+        Random gen = new Random();
         for(int i = 0; i < 5 ; i++) {
-            cannons.add(new Cannon());
+            cannons.add(new Cannon(gen.nextBoolean()));
         }
 
-        for (Cannon c : cannons) {
-            c.fire();
-        }
+        Cannon.text("Fire cannons:");
+        cannons.stream().forEach(c -> c.fire());
 
-        for (Cannon c : cannons) {
-            c.fire();
-        }
+        Cannon.text("Cannons status:");
+        cannons.stream().forEach(c -> c.status());
+
+        Cannon.text("Loading unloaded cannons");
+        cannons.stream().filter(c -> !c.isLoaded()).forEach(c -> c.loadCanon());
+
+        Cannon.text("Cannons status:");
+        cannons.stream().forEach(c -> c.status());
+
+        Cannon.text("Fire cannons:");
+        cannons.stream().forEach(c -> c.fire());
+
+        Cannon.text("Cannons status:");
+        cannons.stream().forEach(c -> c.status());
+
     }
 }
